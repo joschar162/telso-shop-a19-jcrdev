c04b86c8e20de7a3988f3b838d28cf4ef68a1bc1
josdev <chaqquererj@gmail.com>
2025-12-04 12:54:48 -0500

    feat: rutas y pagina de autenticacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+
+    //todo: gards
+  },
+
   { path: '', loadChildren: () => import('./store-front/store-front.routes') },
 ];
