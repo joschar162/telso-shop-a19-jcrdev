fc43a5ea22fa4783059e2e92ce17b0a24cfdd3d1
josdev <chaqquererj@gmail.com>
2026-01-07 12:20:58 -0500

    feat(admin): implementar guard de autorización por roles

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -31,6 +31,7 @@ export class AuthService {
 
   user = computed<User | null>(() => this._user());
   token = computed<string | null>(this._token);
+  isAdmin = computed(() => this._user()?.roles.includes('admin') ?? false);
 
   login(email: string, password: string): Observable<boolean> {
     return this.http
